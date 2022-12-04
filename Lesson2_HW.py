a68f759ef9a823e8da50b24f2be1fbcd164c7c92
Heorhii <george.shalolashvili2005@gmail.com>
2022-12-04 12:33:30 -0500

    Fourth task

--- Lesson2_HW.py
+++ Lesson2_HW.py
@@ -1,8 +1,8 @@
-import_antigravity = "import antigravity"
-print(import_antigravity)
+print('*\t***  \t*\t*****    ')
+print('*\t      \t*')
+print('*\t***    \t*')
+print('*\t       \t*')
+print('*\t***   \t*')
 
-import_hello_ = "import _hello_"
-print(import_hello_)
 
-from_future = "from _future_ import braces"
-print(from_future)
+
